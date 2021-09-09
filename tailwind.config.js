d7dcb9a121e8c74ef2269702b567991acdfbcbe5
Héctor Villalobos <h.villalobosm03@gmail.com>
2021-09-09 12:10:58 -0300

    configurando tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+
+    purge: [
+        './public/**/*.html',
+    ],
+    darkMode: false,
+    theme: {
+        extend: {}
+    },
+    variants: {
+        extend: {}
+    },
+    plugins: [],
+
+}
